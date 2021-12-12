020e50e93e1be0506c15444eb8917c8b333547b2
Matthew Bardsley <Matthew.Bardsley@warwick.ac.uk>
2021-12-12 21:47:39 +0000

    add new command

---
+++ conversions.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"bytes"
+	"encoding/gob"
+)
+
+func getBytes(data interface{}) ([]byte, error) {
+	var buf bytes.Buffer
+	enc := gob.NewEncoder(&buf)
+	err := enc.Encode(data)
+	if err != nil {
+		return nil, err
+	}
+	return buf.Bytes(), nil
+}
--- main.go
+++ main.go
@@ -2,8 +2,61 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+
+	"github.com/mhbardsley/todo/todo"
 )
+
+// NewTodo initialises a todo file in the user's home folder
+func NewTodo() {
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		log.Fatal(err)
+	}
+	newFilePath := filepath.Join(homeDir, ".todolist")
+	_, err = os.Create(newFilePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	var list todo.List
+	b, err := getBytes(list)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = ioutil.WriteFile(newFilePath, b, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+// AddTodo adds to the existing todo list
+func AddTodo() {
+}
+
+// GetTodo gets from the existing todo list
+func GetTodo() {
+}
+
+// GetTodo retrieves from the existing todo list
 
 func main() {
 	fmt.Println("Hello world!")
+
+	command := os.Args[1]
+
+	// process the command
+	// new initialises file in home folder; add puts a new item on the todo list; get retrieves the next item on the todo list
+	switch command {
+	case "new":
+		NewTodo()
+	case "add":
+		AddTodo()
+	case "get":
+		GetTodo()
+	default:
+		fmt.Println("Command not recognised")
+	}
 }
